fbdb68ef7e5159b8885f7e48021bcc0f94208ba2
jorgeromeromg2 <78120390+jorgeromeromg2@users.noreply.github.com>
2021-06-08 21:37:18 -0300

    Atualizando

---
+++ mundo3/ex114.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib.request
+
+
+try:
+    site = urllib.request.urlopen\
+        ('http://www.feijaodeouro.com.br/')
+except urllib.error.URLError:
+    print('O site não está acessível no momento')
+else:
+    print('Consegui acessar o site com sucesso.')
